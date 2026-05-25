baf0d89b0d1a7200e46091f04b81ca96a861cd6f
Xyrelix <officialoladayooladoyin@gmail.com>
2026-05-25 17:32:56 +0100

    port number change

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 4000;
+
 
 // Serve frontend files
 app.use(express.static(path.join(__dirname, '../frontend')));
